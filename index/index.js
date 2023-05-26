d0b9fd0210880966dd148bc21d19a03fd6728c95
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-26 10:49:26 -0400

    added functions and more css to buttons

--- index/index.js
+++ index/index.js
@@ -1,1 +1,24 @@
+$(document).ready(function () {
+  var alertsOpen = false;
+  var lastClickedTab = null;
 
+  $(".tabs button").click(function () {
+    if (lastClickedTab) {
+      lastClickedTab.removeClass("highlight");
+    }
+
+    $(this).addClass("highlight");
+
+    lastClickedTab = $(this);
+  });
+
+  $("#alertButton").click(function () {
+    alertsOpen = !alertsOpen;
+
+    if (alertsOpen) {
+      $(".alerts").addClass("open");
+    } else {
+      $(".alerts").removeClass("open");
+    }
+  });
+});
