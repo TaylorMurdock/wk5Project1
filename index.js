1e17144273a9364da15d5748372007ec860d2c4b
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-30 22:19:01 -0400

    working on api and content box along w/ background

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const rainImgPath = "url(./imgs/rainingImg.jpeg)";
+const cloudImgPath = "url(./imgs/cloudyImg.jpeg)";
+const snowImgPath = "url(./imgs/snowImg.jpeg)";
+const sunnyImgPath = "url(./imgs/sunnyImg.avif)";
+const nightImgPath = "url(./imgs/nightImg.jpeg)";
+
 $(document).ready(function () {
   var alertsOpen = false;
   var lastClickedTab = null;
@@ -23,33 +29,66 @@ $(document).ready(function () {
     }
   });
 });
-
+// lat =35.590310
+// lon = -82.487800
+// need to make weatherUrl dynamic
 const currentWeatherUrl =
-  "https://api.openweathermap.org/data/2.5/weather?lat=35.590310&lon=-82.487800&appid=20c52dcce61a134a5c6deb567556e70c&units=imperial";
+  "https://api.openweathermap.org/data/2.5/weather?lat=19.8987&lon=155.6659&appid=20c52dcce61a134a5c6deb567556e70c&units=imperial";
+
+// grab the search bar input value
+
+// when you submit the search do a bunch of things
+
+// create a function that takes a parameter. plug the input value from the search bar into the parameter and make an api call to zipcode api http://api.openweathermap.org/geo/1.0/zip?zip={zipcode}&appid={API key} replacing {zipcode} with the input value and {API key} with api key
 
 fetch(currentWeatherUrl)
   .then((response) => {
-    // console.log(response.json());
     return response.json();
   })
   .then((data) => {
-    console.log();
-    const weather = data.main;
+    console.log(data);
+    const main = data.main;
+    const weatherDesc = data.weather[0].main;
+    const time = data.sys;
+    console.log(time);
 
     const content = $(".content");
-    // console.log(content);
+
     content.html(`
     <div class="curTemp">
-    <b>Current Temperature:&nbsp;</b> ${weather.temp}
+    <b>Current Temperature:&nbsp;</b> ${main.temp}
     </div>
-     <div>
-    <b>Feels Like:&nbsp;</b>${weather.feels_like}
+     <div class="curFeelsLike">
+    <b>Feels Like:&nbsp;</b>${main.feels_like}
     </div>
-     <div>
-     <b>Low for the day:&nbsp;</b>${weather.temp_min}
+     <div class="curLow">
+     <b>Low for the day:&nbsp;</b>${main.temp_min}
     </div>
-         <div>
-     <b>High for the day:&nbsp;</b>${weather.temp_max}
+    <div class="curHigh">
+     <b>High for the day:&nbsp;</b>${main.temp_max}
     </div>
     `);
+
+    function changeBackgroundImg(weatherDesc, time) {
+      const body = $("body");
+
+      if (weatherDesc === "Clouds") {
+        body.css("background-image", cloudImgPath);
+      } else if (weatherDesc === "Rain") {
+        body.css("background-image", rainImgPath);
+      } else if (weatherDesc === "Snow") {
+        body.css("background-image", snowImgPath);
+      } else if (
+        weatherDesc !== "Rain" &&
+        weatherDesc !== "Snow" &&
+        weatherDesc !== "Clouds"
+      ) {
+        body.css("background-image", sunnyImgPath);
+      } else {
+        body.css("background-image", nightImgPath);
+      }
+      // need to add sunny img and night time img depending on sunset and sunrise returns
+    }
+
+    changeBackgroundImg(weatherDesc);
   });
