3615915cdfbb4ffd59581c846dca714fd4f4bce5
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-31 20:44:38 -0400

    more css for media

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-const rainImgPath = "url(./imgs/rainingImg.jpeg)";
-const cloudImgPath = "url(./imgs/cloudyImg.jpeg)";
-const snowImgPath = "url(./imgs/snowImg.jpeg)";
-const sunnyImgPath = "url(./imgs/sunnyImg.avif)";
-const nightImgPath = "url(./imgs/nightImg.jpeg)";
+const rainImgPath = "url(./imgs/rainingImg.jpeg)"; // Assigns the path to the raining image
+const cloudImgPath = "url(./imgs/cloudyImg.jpeg)"; // Assigns the path to the cloudy image
+const snowImgPath = "url(./imgs/snowImg.jpeg)"; // Assigns the path to the snow image
+const sunnyImgPath = "url(./imgs/sunnyImg.avif)"; // Assigns the path to the sunny image
+const nightImgPath = "url(./imgs/nightImg.jpeg)"; // Assigns the path to the night image
 
 $(document).ready(function () {
-  var alertsOpen = false;
-  var lastClickedTab = null;
+  var alertsOpen = false; // Initializes a variable to keep track of whether alerts are open or closed
+  var lastClickedTab = null; // Initializes a variable to store the last clicked tab
 
-  $(".tabs button").eq(0).addClass("highlight");
+  $(".tabs button").eq(0).addClass("highlight"); // Adds the "highlight" class to the first button in the tabs section
 
   $(".tabs button").click(function () {
-    $(".tabs button").eq(0).removeClass("highlight");
+    $(".tabs button").eq(0).removeClass("highlight"); // Removes the "highlight" class from the first button in the tabs section
 
     if (lastClickedTab) {
-      lastClickedTab.removeClass("highlight");
+      lastClickedTab.removeClass("highlight"); // Removes the "highlight" class from the previously clicked button
     }
 
-    $(this).addClass("highlight");
-    lastClickedTab = $(this);
+    $(this).addClass("highlight"); // Adds the "highlight" class to the currently clicked button
+    lastClickedTab = $(this); // Stores the currently clicked button as the last clicked tab
   });
 
   $("#alertButton").click(function () {
-    alertsOpen = !alertsOpen;
+    alertsOpen = !alertsOpen; // Toggles the value of alertsOpen variable
 
     if (alertsOpen) {
-      $(".alerts").addClass("open");
-      $(this).addClass("highlight");
+      $(".alerts").addClass("open"); // Adds the "open" class to the alerts section
+      $(this).addClass("highlight"); // Adds the "highlight" class to the alertButton
     } else {
-      $(".alerts").removeClass("open");
-      $(this).removeClass("highlight");
+      $(".alerts").removeClass("open"); // Removes the "open" class from the alerts section
+      $(this).removeClass("highlight"); // Removes the "highlight" class from the alertButton
     }
   });
 
   $(".weeklyWeather").click(function () {
-    const content = $(".content");
+    const content = $(".content"); // Selects the content section
 
-    content.empty();
+    content.empty(); // Clears the content section
 
     const defaultOneCall =
       "https://api.openweathermap.org/data/3.0/onecall?lat=35.590310&lon=-82.487800&appid=20c52dcce61a134a5c6deb567556e70c&units=imperial";
+    // Assigns the API URL for fetching weekly weather data
 
     fetch(defaultOneCall)
       .then((response) => {
-        return response.json();
+        return response.json(); // Returns the response as JSON
       })
       .then((data) => {
-        const weeklyData = data.daily;
-        console.log(weeklyData);
+        const weeklyData = data.daily; // Extracts the daily weather data from the response
 
         weeklyData.map((dailyData) => {
           const newElement = `<div class="weeklyWeather> 
@@ -55,30 +55,32 @@ $(document).ready(function () {
     <div>Max Temp: ${dailyData.temp.max}</div>
     <div>Summary: ${dailyData.summary}</div>
     </div>`;
-          content.append(newElement);
+          // Creates a new element with the daily weather data
+
+          content.append(newElement); // Appends the new element to the content section
         });
       });
   });
 
   $(".currentWeather").click(function () {
-    const content = $(".content");
+    const content = $(".content"); // Selects the content section
 
-    content.empty();
+    content.empty(); // Clears the content section
 
     const defaultCurrentWeatherUrl =
       "https://api.openweathermap.org/data/2.5/weather?lat=35.590310&lon=-82.487800&appid=20c52dcce61a134a5c6deb567556e70c&units=imperial";
+    // Assigns the API URL for fetching current weather data
 
     fetch(defaultCurrentWeatherUrl)
       .then((response) => {
-        return response.json();
+        return response.json(); // Returns the response as JSON
       })
       .then((data) => {
-        console.log(data);
-        const main = data.main;
-        const weatherDesc = data.weather[0].main;
-        const time = data.sys;
+        const main = data.main; // Extracts the main weather data from the response
+        const weatherDesc = data.weather[0].main; // Extracts the weather description from the response
+        const time = data.sys; // Extracts the time data from the response
 
-        const content = $(".content");
+        const content = $(".content"); // Selects the content section again
 
         content.html(`
     <div class="cityName">
@@ -97,57 +99,58 @@ $(document).ready(function () {
      <b>High for the day:&nbsp;</b>${main.temp_max}
     </div>
     `);
+        // Updates the content section with the current weather data
 
         function changeBackgroundImg(weatherDesc) {
-          const body = $("body");
-          const currentTime = Math.floor(Date.now() / 1000);
+          const body = $("body"); // Selects the body element
+          const currentTime = Math.floor(Date.now() / 1000); // Gets the current time
 
           if (currentTime < time.sunrise || currentTime > time.sunset) {
-            body.css("background-image", nightImgPath);
+            body.css("background-image", nightImgPath); // Sets the background image to the night image if it's nighttime
           } else if (weatherDesc === "Clouds") {
-            body.css("background-image", cloudImgPath);
+            body.css("background-image", cloudImgPath); // Sets the background image to the cloudy image if it's cloudy
           } else if (weatherDesc === "Rain") {
-            body.css("background-image", rainImgPath);
+            body.css("background-image", rainImgPath); // Sets the background image to the raining image if it's raining
           } else if (weatherDesc === "Snow") {
-            body.css("background-image", snowImgPath);
+            body.css("background-image", snowImgPath); // Sets the background image to the snow image if it's snowing
           } else if (
             weatherDesc !== "Rain" &&
             weatherDesc !== "Snow" &&
             weatherDesc !== "Clouds"
           ) {
-            body.css("background-image", sunnyImgPath);
+            body.css("background-image", sunnyImgPath); // Sets the background image to the sunny image if it's clear weather
           }
         }
 
-        changeBackgroundImg(weatherDesc);
+        changeBackgroundImg(weatherDesc); // Calls the function to change the background image based on the weather description
       });
   });
 
   $(".zipSubmit").click(function () {
-    const input = $(".zipInput").val();
+    const input = $(".zipInput").val(); // Retrieves the value from the zip input field
+
     const convertZipAndChangeTemp = (zip) => {
       fetch(
         `http://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=20c52dcce61a134a5c6deb567556e70c`
       )
         .then((response) => {
-          return response.json();
+          return response.json(); // Returns the response as JSON
         })
         .then((data) => {
-          console.log(data);
-          const cityName = data.name;
+          const cityName = data.name; // Extracts the city name from the response
 
           fetch(
             `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=20c52dcce61a134a5c6deb567556e70c&units=imperial`
           )
             .then((response) => {
-              return response.json();
+              return response.json(); // Returns the response as JSON
             })
             .then((data) => {
-              console.log(data);
-              const content = $(".content");
-              const weatherDesc = data.weather[0].main;
-              const main = data.main;
-              const time = data.sys;
+              const content = $(".content"); // Selects the content section again
+              const weatherDesc = data.weather[0].main; // Extracts the weather description from the response
+              const main = data.main; // Extracts the main weather data from the response
+              const time = data.sys; // Extracts the time data from the response
+
               content.html(`
                 <div class="cityName">
                 <b>Current Location:&nbsp;</b> ${cityName}
@@ -165,47 +168,49 @@ $(document).ready(function () {
                 <b>High for the day:&nbsp;</b>${main.temp_max}
                 </div>
               `);
+              // Updates the content section with the current weather data
+
               function changeBackgroundImg(weatherDesc) {
-                const body = $("body");
+                const body = $("body"); // Selects the body element
 
                 if (weatherDesc === "Clouds") {
-                  body.css("background-image", cloudImgPath);
+                  body.css("background-image", cloudImgPath); // Sets the background image to the cloudy image if it's cloudy
                 } else if (weatherDesc === "Rain") {
-                  body.css("background-image", rainImgPath);
+                  body.css("background-image", rainImgPath); // Sets the background image to the raining image if it's raining
                 } else if (weatherDesc === "Snow") {
-                  body.css("background-image", snowImgPath);
+                  body.css("background-image", snowImgPath); // Sets the background image to the snow image if it's snowing
                 } else if (
                   weatherDesc !== "Rain" &&
                   weatherDesc !== "Snow" &&
                   weatherDesc !== "Clouds"
                 ) {
-                  body.css("background-image", sunnyImgPath);
+                  body.css("background-image", sunnyImgPath); // Sets the background image to the sunny image if it's clear weather
                 }
               }
 
-              changeBackgroundImg(weatherDesc);
+              changeBackgroundImg(weatherDesc); // Calls the function to change the background image based on the weather description
             });
         });
     };
 
-    convertZipAndChangeTemp(input);
+    convertZipAndChangeTemp(input); // Calls the function to fetch and update weather data based on the input zip code
   });
 });
 
 const defaultCurrentWeatherUrl =
   "https://api.openweathermap.org/data/2.5/weather?lat=35.590310&lon=-82.487800&appid=20c52dcce61a134a5c6deb567556e70c&units=imperial";
+// Assigns the API URL for fetching current weather data
 
 fetch(defaultCurrentWeatherUrl)
   .then((response) => {
-    return response.json();
+    return response.json(); // Returns the response as JSON
   })
   .then((data) => {
-    console.log(data);
-    const main = data.main;
-    const weatherDesc = data.weather[0].main;
-    const time = data.sys;
+    const main = data.main; // Extracts the main weather data from the response
+    const weatherDesc = data.weather[0].main; // Extracts the weather description from the response
+    const time = data.sys; // Extracts the time data from the response
 
-    const content = $(".content");
+    const content = $(".content"); // Selects the content section again
 
     content.html(`
     <div class="cityName">
@@ -224,27 +229,28 @@ fetch(defaultCurrentWeatherUrl)
      <b>High for the day:&nbsp;</b>${main.temp_max}
     </div>
     `);
+    // Updates the content section with the current weather data
 
     function changeBackgroundImg(weatherDesc) {
-      const body = $("body");
-      const currentTime = Math.floor(Date.now() / 1000);
+      const body = $("body"); // Selects the body element
+      const currentTime = Math.floor(Date.now() / 1000); // Retrieves the current time
 
       if (currentTime < time.sunrise || currentTime > time.sunset) {
-        body.css("background-image", nightImgPath);
+        body.css("background-image", nightImgPath); // Sets the background image to the night image if it's nighttime
       } else if (weatherDesc === "Clouds") {
-        body.css("background-image", cloudImgPath);
+        body.css("background-image", cloudImgPath); // Sets the background image to the cloudy image if it's cloudy
       } else if (weatherDesc === "Rain") {
-        body.css("background-image", rainImgPath);
+        body.css("background-image", rainImgPath); // Sets the background image to the raining image if it's raining
       } else if (weatherDesc === "Snow") {
-        body.css("background-image", snowImgPath);
+        body.css("background-image", snowImgPath); // Sets the background image to the snow image if it's snowing
       } else if (
         weatherDesc !== "Rain" &&
         weatherDesc !== "Snow" &&
         weatherDesc !== "Clouds"
       ) {
-        body.css("background-image", sunnyImgPath);
+        body.css("background-image", sunnyImgPath); // Sets the background image to the sunny image if it's clear weather
       }
     }
 
-    changeBackgroundImg(weatherDesc);
+    changeBackgroundImg(weatherDesc); // Calls the function to change the background image based on the weather description
   });
